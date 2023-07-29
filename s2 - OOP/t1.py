185dbf9796dc13076f0baea6a438366c07278d65
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-07-29 16:02:18 +0330

    Update t1.py
    add the question to the file.

--- s2 - OOP/t1.py
+++ s2 - OOP/t1.py
@@ -1,3 +1,32 @@
+'''In schools A and B, two different health programs have been considered.
+In one school, milk is distributed, while in the other school it is not. Nutrition experts intend to compare these two classes by examining the height, weight, and age of the students.
+Write a program that takes the number of students in each class, along with their age, height, and weight, and stores them in separate lists. 
+Then, print the average age, height, and weight of each class on separate lines (printed as floats). Finally, print the class with the highest average height.
+If the average heights are equal, print the class with the lower average weight. If both the average height and weight are equal, print "Same" exactly as written.
+Using a class in solving this exercise is necessary.
+
+input :
+5
+16 17 15 16 17
+180 175 172 170 165
+67 72 59 62 55
+5
+15 17 16 15 16
+166 156 168 170 162
+45 52 56 58 47
+
+output :
+16.2
+172.4
+63.0
+15.8
+164.4
+51.6
+A
+
+'''
+
+
 import statistics
 
 class Students:
