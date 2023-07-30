c0790ed4c7109eb5777c19a1b3e9e0fa6b511928
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-07-30 13:48:27 +0330

    Update and rename t1.py to t1_Health Program Comparison in Schools A and B.py
    add comments and rename file

--- s2 - OOP/t1_Health Program Comparison in Schools A and B.py
+++ s2 - OOP/t1_Health Program Comparison in Schools A and B.py
@@ -1,51 +1,30 @@
-'''In schools A and B, two different health programs have been considered.
-In one school, milk is distributed, while in the other school it is not. Nutrition experts intend to compare these two classes by examining the height, weight, and age of the students.
-Write a program that takes the number of students in each class, along with their age, height, and weight, and stores them in separate lists. 
-Then, print the average age, height, and weight of each class on separate lines (printed as floats). Finally, print the class with the highest average height.
-If the average heights are equal, print the class with the lower average weight. If both the average height and weight are equal, print "Same" exactly as written.
-Using a class in solving this exercise is necessary.
-
-input :
-5
-16 17 15 16 17
-180 175 172 170 165
-67 72 59 62 55
-5
-15 17 16 15 16
-166 156 168 170 162
-45 52 56 58 47
-
-output :
-16.2
-172.4
-63.0
-15.8
-164.4
-51.6
-A
-
-'''
-
-
 import statistics
 
+# Define the Students class
 class Students:
     count = 0
+
+    # Constructor to initialize student attributes
     def __init__(self , sen , ghad , vazn , count):
         self.sen = sen
         self.ghad = ghad
         self.vazn = vazn
         Students.count = count
+    
+    # Calculate the average age of students
     def aveOfSen(self):
         return(statistics.mean(self.sen))
+
+    # Calculate the average height of students
     def aveOfGhad(self):
         return(statistics.mean(self.ghad))
 
+    # Calculate the average weight of students
     def aveOfVazn(self):
         return(statistics.mean(self.vazn))
 
     
-
+# Input and process data for students of class A
 n = input()    
 listOfSen =input().split()
 listOfGhad =input().split()
@@ -53,10 +32,9 @@ listOfVazn =input().split()
 listOfSen= list(map(int , listOfSen))
 listOfVazn= list(map(int , listOfVazn))
 listOfGhad= list(map(int , listOfGhad))
-
 studentsA = Students(listOfSen,listOfGhad,listOfVazn,n)
 
-
+# Input and process data for students of class B
 n = input()    
 listOfSen =input().split()
 listOfGhad =input().split()
@@ -64,24 +42,26 @@ listOfVazn =input().split()
 listOfSen= list(map(int , listOfSen))
 listOfVazn= list(map(int , listOfVazn))
 listOfGhad= list(map(int , listOfGhad))
-
 studentsB = Students(listOfSen,listOfGhad,listOfVazn,n)
 
-
+# Calculate and print the average age, height, and weight for class A
 aveOfSenA =studentsA.aveOfSen()
 aveOfGhadA = studentsA.aveOfGhad()
 aveOfVaznA = studentsA.aveOfVazn()
-aveOfSenB = studentsB.aveOfSen()
-aveOfGhadB = studentsB.aveOfGhad()
-aveOfVaznB = studentsB.aveOfVazn()
-
 print(float(aveOfSenA))
 print(float(aveOfGhadA))
 print(float(aveOfVaznA))
+
+# Calculate and print the average age, height, and weight for class B
+aveOfSenB = studentsB.aveOfSen()
+aveOfGhadB = studentsB.aveOfGhad()
+aveOfVaznB = studentsB.aveOfVazn()
 print(float(aveOfSenB))
 print(float(aveOfGhadB))
 print(float(aveOfVaznB))
 string = "Same"
+
+# Compare average heights and weights of both classes to determine the class with the highest average height
 if aveOfGhadA!=aveOfGhadB:
     if aveOfGhadB < aveOfGhadA :
         string = "A"
@@ -92,5 +72,6 @@ elif aveOfVaznA!= aveOfVaznB :
         string = "A"
     else:
         string = "B"
-
+        
+# Print the result indicating the class with the highest average height or "Same" if both are equal
 print(string)
