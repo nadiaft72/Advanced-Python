815f73c5ce098294a1de3a589ce09767583da6b6
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-07-30 16:14:56 +0330

    Update lblEnc.py
    add comments

--- s6 - final project ( Web scraping and use ML)/lblEnc.py
+++ s6 - final project ( Web scraping and use ML)/lblEnc.py
@@ -1,5 +1,6 @@
-# ML :
-from bs4.element import PYTHON_SPECIFIC_ENCODINGS
+# lblEnc.py - Label Encoding and Machine Learning
+
+# Importing necessary librariesfrom bs4.element import PYTHON_SPECIFIC_ENCODINGS
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
@@ -9,23 +10,28 @@ import csv
 from sklearn import tree
 from sklearn.preprocessing import LabelEncoder
 
-
+# Reading data from 'dataB.csv' using pandas
 cr = pd.read_csv("E:\workplace\python\Advanced Python\s6\dataB.csv")
 
-
+# Label Encoding for categorical columns
 label_encoders = {}
 categorical_columns = ["model_name" ,"model"  ,"accident","location", "year" ]
 
+# Perform label encoding for each categorical column and transform the data
 for column in categorical_columns:
     label_encoders[column] = LabelEncoder()
     cr[column] = label_encoders[column].fit_transform(cr[column]) 
 
+# Splitting the first row of the dataset into individual data points
 x = np.split(cr.values[0] ,7 )
 for line in x:
     print(int(line))
+
+# Initializing empty lists x and y to store feature and target data
 x=[]
 y=[]
 
+# Splitting the data into features (x) and target (y)
 for line in cr.values:
     z = np.split(line ,7)
     w=[]
@@ -34,6 +40,7 @@ for line in cr.values:
     x.append(line[0:6])
     y.append(line[6])
 
+# Creating a Decision Tree classifier and fitting the data
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x,y)
 
