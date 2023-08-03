4da8e3b1251cf78f1b71540d9b994e046f52d8ba
Nadia Fallahtafti <33514608+nadiaft72@users.noreply.github.com>
2023-08-04 00:34:47 +0330

    Update MLexp.py
    add comments

--- s6 - final project ( Web scraping and use ML)/MLexp.py
+++ s6 - final project ( Web scraping and use ML)/MLexp.py
@@ -15,8 +15,6 @@ import numpy as np
 # Read the car data from the CSV file 'dataB.csv' using pandas.
 cr = pd.read_csv("E:\workplace\python\Advanced Python\s6\dataB.csv")
 
-# Ï?ÊÇ? ÌÏ?Ï Ñæ ÇÒ ˜ÇÑÈÑ ã??Ñå æ Èå ÝÇ?á ÇÖÇÝå ã?˜äÏ
-# æ æÞÊ? ÝÇ?á ÏÇÑå ˜Ï ã?Ôå ÇØáÇÚÇÊ ÌÏ?Ï ˜ÇÑÈÑ åã ÈÇåÇÔ ˜Ï ã?Ôå ÊÇ ÈÊæä?ã ÈÚÏÔ Ï?ÊÇ? ˜Ï ÔÏå ÑÇ ÈÏ?ã Èå ÑÏ?˜Ê
 # Collect new car data from the user or use the default data (Acura, SH-AWD, 1046, 2, CA, 2019, 0).
 new_data = "Acura,SH-AWD,1046,2,CA,2019,0"
 yOrn = input("We considered the following data : Acura,SH-AWD,1046,2,CA,2019,0 , if you wanna add another data enter y else enter n :")
@@ -32,17 +30,13 @@ with open('E:\workplace\python\Advanced Python\s6\dataB.csv','a') as fd:
 # Encode categorical columns ('model_name', 'model', 'accident', 'location', 'year') using LabelEncoder.
 label_encoders = {}
 categorical_columns = ["model_name" ,"model"  ,"accident","location", "year" ]
-
-#˜Ï ÐÇÑ? ã?˜ä?ã æä ÏÓ?Žä ÊÑ? ÇÓÊÑ?ä ÞÈæá äã?˜äå 
 for column in categorical_columns:
     label_encoders[column] = LabelEncoder()
     cr[column] = label_encoders[column].fit_transform(cr[column]) 
 
-# ÍÇáÇ ˜å ˜Ï ÐÇÑ? ÔÏ ã?Ç?ã æ æÑæÏ? æ ÎÑæÌ? Ï?Ó?Žä ÊÑ? Ñæ ãÞÏÇÑ Ïå? ã?˜ä?ã
 # Prepare features (x) and target variable (y) for the Machine Learning model.
 x=[]
 y=[]
-
 for line in cr.values:
     z = np.split(line ,7)
     w=[]
@@ -51,28 +45,26 @@ for line in cr.values:
     x.append(line[0:6])
     y.append(line[6])
     
-#ÂÎÑ?ä ÓØÑ æÇÑÏ ÔÏå Ï?ÊÇ? ÌÏ?Ïå Ó ÇÒ æÑæÏ? æ ÎÑæÌ? åÇ Ç ã?˜ä?ã   
 # Extract the new car data and prepare it for prediction.
 new_data = x.pop()
 y.pop()
 
-#ÂÎÑ?ä ÓØÑ Ñæ ÇÒ ÝÇ?á åã Ç˜ ã?˜ä?ã 
+# Remove the last line from the CSV file to remove the appended new car data.
 f = open('E:\workplace\python\Advanced Python\s6\dataB.csv', "r+")
 lines = f.readlines()
 lines.pop()
 f = open('E:\workplace\python\Advanced Python\s6\dataB.csv', "w+")
 f.writelines(lines)
 
-#ÎÈ ÍÇáÇ ã?Ï?ãÔ Èå Ï?Ó?Žä ÊÑ? ÊÇ ãÏá ÈäÏ?Ôæä ˜äå
+# Train the DecisionTreeClassifier on the dataset and make a price prediction for the new car data.
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x,y)
-
-#Ç?äÌÇ åã ÈÑÇ? Ç?ä˜å ÈÏ?ãÔ Èå ÑÏ?˜ ?å ÂÑÇ?å ÏÑÓÊ ã?ÓÇÒ?ã æ ã?Ï?ãÔ Èå ÑÏ?˜Ê
 list_new_data = []
 for data in new_data:
     list_new_data.append(data)
-
 Answer = clf.predict([list_new_data])
 new_price = Answer[0]
+
+# Display the predicted price for the new car data.
 print("you can use  price for this car : "  )
 print(new_price)
